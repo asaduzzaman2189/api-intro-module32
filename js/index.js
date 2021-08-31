0b13fdd30d0242d4a72989c4cb51165d1745dce4
asaduzzaman2189 <asaduzzaman2189@gmail.com>
2021-08-31 11:25:46 +0600

    32-2 Intro To Json, Json Structure, Parse, Stringify

--- js/index.js
+++ js/index.js
@@ -1,2 +1,26 @@
+//javascript object Notation
+//JSON
 const user = { id: 11, name: 'Gorib Amir', job: 'actor' };
-console.log(user);
+const stringified = JSON.stringify(user);
+// console.log(user);
+// console.log(stringified);
+
+const shop = {
+    name: 'Alia Store',
+    address: 'Ranbir Road',
+    profit: 15000,
+    products: ['laptop', 'mobile', 'pepsi'],
+    owner: {
+        name: 'Alia Bhatt',
+        profession: 'actor'
+    },
+    isExpensive: false
+};
+const shopStringified = JSON.stringify(shop);
+// console.log(shop);
+// console.log(shopStringified);
+console.log(shopStringified.owner);// এভাবে দিলে undefined দেখাবে। আউটপুট দেখাবে না তাই stringify এ এভাবে দেখানো যায় না। 
+const converted = JSON.parse(shopStringified);
+console.log(converted);
+// console.log(converted.owner);// এভাবে দিলেও আউটপুট দেখাবে - {name: "Alia Bhatt", profession: "actor"}
+
